8bbd3c8f6f3c307e065f782671a145fded834765
quangtranho <166036439+quangtranho@users.noreply.github.com>
2024-11-15 13:57:30 +0700

    nộp bài 9

---
+++ ss9/bai_tap/luyen_tap_cau_truc_dieu_kien_01/bai_2/main.js
@@ -0,0 +1,14 @@
+function kiemtra() {
+    let tuoi = document.getElementById('tuoi').value;
+    let result;
+    if (tuoi>=100||tuoi>=1){
+    if (tuoi == 15 || tuoi ==16){
+        result = "Đủ điều kiện vào lớp 10";
+    }else {
+        result = "Không đủ điều kiện vào lớp 10";
+    }}
+    else {
+        result = "Nhập sai tuổi";
+    }
+    document.getElementById('result').innerHTML = result;
+}
